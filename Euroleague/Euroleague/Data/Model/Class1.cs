789c7f0065e5f6834d7de9363b0c9126e2ec2264
Dragan Kos <dragan_kos@outlook.com>
2022-05-03 20:31:32 +0200

    new commit

---
+++ Euroleague/Euroleague/Data/Model/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euroleague.Data.Model
+{
+    internal class Class1
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+    }
+}
